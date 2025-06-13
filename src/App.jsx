8f9d6642813abd704d87b9204f74f7ca942afda4
carolinafrederico <iam@carolinafrederico.com>
2025-06-13 00:15:35 -0400

    New Game

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ import "./index.css"
 export default function App() {
     const [dice, setDice] = useState(generateAllNewDice())
 
+    const gameWon = dice.every(die => die.isHeld) &&
+        dice.every(die => die.value === dice[0].value)
+
     function generateAllNewDice() {
         return new Array(10)
             .fill(0)
@@ -17,28 +20,19 @@ export default function App() {
     }
 
     function rollDice() {
-    setDice(oldDice =>
-        oldDice.map(die =>
-            die.isHeld
-                ? die
-                : {
-                    ...die,
-                    value: Math.ceil(Math.random() * 6),
-                    id: nanoid() // optional: generate new id for new roll
-                }
-        )
-    )
-}
-
+        setDice(oldDice => oldDice.map(die =>
+            die.isHeld ?
+                die :
+                { ...die, value: Math.ceil(Math.random() * 6) }
+        ))
+    }
 
     function hold(id) {
-        setDice(oldDice => {
-            return oldDice.map(die => {
-                return die.id === id ?
-                    {...die, isHeld: !die.isHeld} :
-                    die
-            })
-        })
+        setDice(oldDice => oldDice.map(die =>
+            die.id === id ?
+                { ...die, isHeld: !die.isHeld } :
+                die
+        ))
     }
 
     const diceElements = dice.map(dieObj => (
@@ -52,10 +46,14 @@ export default function App() {
 
     return (
         <main>
+            <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
             <div className="dice-container">
                 {diceElements}
             </div>
-            <button className="roll-dice" onClick={rollDice}>Roll</button>
+            <button className="roll-dice" onClick={rollDice}>
+                {gameWon ? "New Game" : "Roll"}
+            </button>
         </main>
     )
 }
